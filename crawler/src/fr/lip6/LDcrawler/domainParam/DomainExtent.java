1905d7d9f31977243470439a7e83a033e801bde3
cvbrandoe <carmen.brando@gmail.com>
2015-07-16 15:03:16 +0200

    removing unnecessary classes

--- crawler/src/fr/lip6/LDcrawler/domainParam/DomainExtent.java
+++
@@ -1,9 +0,0 @@
-package fr.lip6.LDcrawler.domainParam;
-
-/**
- * Class for defining domain-specific parameters.
- * @author Brando & Frontini - Labex OBVIL - Université Paris-Sorbonne - UPMC LIP6
- */
-public class DomainExtent {
-
-}
