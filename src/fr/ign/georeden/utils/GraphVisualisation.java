f4d2baa0ad84f3846d2415006a9cca2e32f4eff8
Pierre-Henri PARIS <pierrehenri.paris@gmail.com>
2016-05-10 10:27:43 +0200

    Talismane management

--- src/fr/ign/georeden/utils/GraphVisualisation.java
+++ src/fr/ign/georeden/utils/GraphVisualisation.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 import org.jgraph.JGraph;
 import org.jgrapht.ext.JGraphModelAdapter;
@@ -12,6 +13,7 @@ import org.jgrapht.graph.SimpleDirectedGraph;
 
 import com.jgraph.layout.JGraphFacade;
 import com.jgraph.layout.organic.JGraphSelfOrganizingOrganicLayout;
+import com.jgraph.layout.hierarchical.JGraphHierarchicalLayout;
 
 /**
  * Permet de visualiser le graphe dans une fenêtre.
@@ -31,6 +33,25 @@ public class GraphVisualisation<V, E> extends JFrame {
 		layout.setMaxIterationsMultiple(10000); // 20
 		layout.setMinRadius(50); // 1
 		layout.run(facade);
+		final Map nestedMap = facade.createNestedMap(true, true);
+		jgraph.getGraphLayoutCache().edit(nestedMap);
+
+		JPanel container = new JPanel();
+		JScrollPane scrPane = new JScrollPane(container);
+		add(scrPane);
+		container.add(jgraph);
+	}
+	
+	public GraphVisualisation(SimpleDirectedGraph<V, E> graph, Object[] roots, String graphName) {
+		super(graphName);
+		
+		JGraph jgraph = new JGraph(new JGraphModelAdapter<V, E>(graph));
+		JGraphFacade facade = new JGraphFacade(jgraph, roots);
+		JGraphHierarchicalLayout layout = new JGraphHierarchicalLayout();
+		layout.setOrientation(SwingConstants.SOUTH);
+		layout.setDeterministic(true);
+		layout.run(facade);
+		@SuppressWarnings("rawtypes")
 		final Map nestedMap = facade.createNestedMap(true, true);
 		jgraph.getGraphLayoutCache().edit(nestedMap);
 
